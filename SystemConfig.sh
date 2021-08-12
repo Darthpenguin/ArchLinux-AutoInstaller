8e866d700cfb4aff269b43f832d839c22809e01d
Darthpenguin <jeff@jeffpaxton.com>
2021-08-12 15:03:32 -0400

    Update SystemConfig.sh

--- SystemConfig.sh
+++ SystemConfig.sh
@@ -3,3 +3,5 @@ pacman -S cups xorg-server xorg-twm xterm xcursor-vanilla-dmz xf86-input-libinpu
 pacman -S cinnamon arc-gtk-theme xed xreader lightdm lightdm-gtk-greeter xdg-user-dirs xdg-user-dirs-gtk
 pacman -S cheese baobab simple-scan file-roller gnome-calculator gnome-disk-utility yelp gnome-screenshot gnome-sound-recorder gnome-terminal gnome-logs 
 pacman -S meson
+
+pacaur -S archlinux-appstream-data-pamac gnome-logs-git google-chrome lightdm-settings lightdm-slick-greeter numix-circle-icon-theme-git numix-folders-git pamac-all pix xplayer xviewer
