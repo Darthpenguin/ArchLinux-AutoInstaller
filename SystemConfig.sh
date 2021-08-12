536eab8f69e63a1dd4d823488019974d9b62ab4d
Darthpenguin <jeff@jeffpaxton.com>
2021-08-12 14:00:50 -0400

    Update SystemConfig.sh

--- SystemConfig.sh
+++ SystemConfig.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 pacman -S cups xorg-server xorg-twm xterm
-pacman -S cinnamon arc-gtk-theme xed xreader
-pacman -S cheese baobab simple-scan file-roller gnome-calculator gnome-disk-utility yelp gnome-screenshot gnome-sound-recorder 
+pacman -S cinnamon arc-gtk-theme xed xreader lightdm lightdm-gtk-greeter
+pacman -S cheese baobab simple-scan file-roller gnome-calculator gnome-disk-utility yelp gnome-screenshot gnome-sound-recorder gnome-terminal gnome-logs 
