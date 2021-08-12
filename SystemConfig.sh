f716d8b11d155c7b46ccc7933de23a9e42e5aee5
Darthpenguin <jeff@jeffpaxton.com>
2021-08-12 13:58:10 -0400

    Create SystemConfig.sh

---
+++ SystemConfig.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+pacman -S cups xorg-server xorg-twm xterm
+pacman -S cinnamon arc-gtk-theme xed xreader
+pacman -S cheese baobab simple-scan file-roller gnome-calculator gnome-disk-utility yelp gnome-screenshot gnome-sound-recorder 
